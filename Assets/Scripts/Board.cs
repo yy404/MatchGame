2981d38f658462b1ec413dc9cbf0e40b9b4a1dfb
yy404 <yuhui.yaocn@gmail.com>
2020-06-09 12:42:28 +0100

    Set board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        SetUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i,j);
+                Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            }
+        }
+    }
+}
